88676a90dd19f41d1a9d5ddbff09fb00297f95b7
heejinshin <86960303+heejinshin@users.noreply.github.com>
2023-05-30 21:30:07 +0900

    update

--- local_people/board/views.py
+++ local_people/board/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+    print("")
+    # boardList = Board.objects.all()
+    # boardList = Board.objects.order_by('-id')
+    # return render(request, "board/board_list.html", {'boardList':boardList} )
---
+++ local_people/member/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        # 사용자가 입력한 PW와 실제 PW의 비교 
+        if request.POST['password1'] == request.POST["password2"]:
+            user = User.objects.create_user(
+                  username = request.POST['username'],
+                password = request.POST['password1'],
+                email = request.POST['email'],
+            )
+            auth.login(request, user)
+            return redirect('/')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
